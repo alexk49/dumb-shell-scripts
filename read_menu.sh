ac78c623b293421e5b0a3d21f42f6412458168cd
alexk49 <alexcode9@mailbox.org>
2023-06-13 08:53:09 +0100

    updated with while loop

--- read_menu.sh
+++ read_menu.sh
@@ -2,44 +2,49 @@
 
 # read menu: a menu driven system information program
 
-clear
-echo "
+DELAY=3 # number of seconds to display results
+
+while [[ "$REPLY" != 0 ]]; do
+
+    clear
+cat <<- _EOF_
 Please Select:
 
 1. Display System Information
 2. Display Disk Space
 3. Display Home Space Utilization
 0. Quit
-"
+_EOF_
 
-read -p "Enter Selection [0-3] > "
+    read -p "Enter Selection [0-3] > "
 
-if [[ "$REPLY" =~ ^[0-3]$ ]]; then
-    if [[ "$REPLY" == 0 ]]; then
-        echo "Program terminated."
-        exit
-    fi
-    if [[ "$REPLY" == 1 ]]; then
-        echo "Hostname: $HOSTNAME"
-        uptime
-        exit
-    fi
-    if [[ "$REPLY" == 2 ]]; then
-        df -h
-        exit
-    fi
-
-    if [[ "$REPLY" == 3 ]]; then
-        if [[ "$(id -u)" -eq 0 ]]; then
-            echo "Home Space Utilization (All Users)"
-            du -sh /home/*
-        else
-            echo "Home Space Utilization ($USER)"
-            du -sh "$HOME"
+    if [[ "$REPLY" =~ ^[0-3]$ ]]; then
+        if [[ "$REPLY" == 0 ]]; then
+            echo "Program terminated."
         fi
-        exit
+        if [[ "$REPLY" == 1 ]]; then
+            echo "Hostname: $HOSTNAME"
+            uptime
+            sleep "$DELAY"
+        fi
+        if [[ "$REPLY" == 2 ]]; then
+            df -h
+            sleep "$DELAY"
+        fi
+
+        if [[ "$REPLY" == 3 ]]; then
+            if [[ "$(id -u)" -eq 0 ]]; then
+                echo "Home Space Utilization (All Users)"
+                du -sh /home/*
+            else
+                echo "Home Space Utilization ($USER)"
+                du -sh "$HOME"
+            fi
+            sleep "$DELAY"
+        fi
+    else
+        echo "Invalid entry." >&2
+        sleep "$DELAY"
     fi
-else
-    echo "Invalid entry." >&2
-    exit 1
-fi
+done
+echo "Program terminated"
